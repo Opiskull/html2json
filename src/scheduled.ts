7ab729a153cd1d44da6f6b707f67b6880f8cd7b2
Opiskull <opi@peerzone.net>
2021-02-22 22:12:27 +0100

    Refactor scheduled

--- src/scheduled.ts
+++ src/scheduled.ts
@@ -1,20 +1,23 @@
-import { RequestBody } from './models'
-import { scrapeHTML } from './scraper'
+import { RequestBody } from './models';
+import { scrapeHTML } from './scraper';
 
 export async function handleScheduled(event: ScheduledEvent) {
   const configs: { [key: string]: RequestBody } =
-    (await CONFIG_STORE.get('configs', 'json')) || {}
+    (await CONFIG_STORE.get('configs', 'json')) || {};
 
-  const date = new Date(event.scheduledTime)
-  date.setSeconds(0)
-  date.setMilliseconds(0)
-  const time = date.toISOString()
+  const date = new Date(event.scheduledTime);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  const time = date.toISOString();
 
-  for (const [key, value] of Object.entries(configs)) {
-    const json = await scrapeHTML(value)
-
-    await JSON_STORE.put(`${key}.${time}`, JSON.stringify(json), {
-      expirationTtl: 172800,
-    })
-  }
+  await Promise.all(
+    Object.entries(configs).map((_) => {
+      const [key, value] = _;
+      return scrapeHTML(value).then((json) =>
+        JSON_STORE.put(`${key}.${time}`, JSON.stringify(json), {
+          expirationTtl: 172800,
+        }),
+      );
+    }),
+  );
 }
